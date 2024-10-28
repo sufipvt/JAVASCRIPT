dfff62f1bdd006294aec2a67769fc9a55201b47a
sufipvt <146200408+sufipvt@users.noreply.github.com>
2024-10-28 06:15:10 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+// alert(3+3)  --  we are not using node js not browser.
+// "use strict" -- treat all js code as newer version.
+
+let name = "sufiyan";
+let age = 20;
+let isloogedin = false;
+
+// number => 2 ** 53
+// bigint
+// string
+// boolean => true / false
+// null = standalone value
+// undefined => value define nhi kri 
+// symbol => unique
+// --------------------------------
+
+// object
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ basics/variable.js
@@ -0,0 +1,3 @@
+const accointId = 1222343
+let accountmail = "sufipvt@gmail.com"
+console.log([accointId,accountmail])
